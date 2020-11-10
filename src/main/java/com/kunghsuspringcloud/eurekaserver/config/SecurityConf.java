1390a369c3e6b422304af45da8e5179beb50caed
xykwin <kunghsu@qq.com>
2020-11-10 22:55:17 +0800

    初始化新工程

---
+++ src/main/java/com/kunghsuspringcloud/eurekaserver/config/SecurityConf.java
@@ -0,0 +1,17 @@
+package com.kunghsuspringcloud.eurekaserver.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConf extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        super.configure(http);
+
+        http.csrf().disable();
+
+    }
+}
