5a38d62c1142b9b230f10e4086cfa32d4b56924a
xykwin <kunghsu@qq.com>
2020-11-11 23:09:20 +0800

    feat:注册中心代码提交

---
+++ src/main/java/cn/com/kun/springcloud/eurekaserver/KunShareEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package cn.com.kun.springcloud.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class KunShareEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KunShareEurekaServerApplication.class, args);
+	}
+
+}
--- src/main/java/cn/com/kun/springcloud/eurekaserver/config/SecurityConf.java
+++ src/main/java/cn/com/kun/springcloud/eurekaserver/config/SecurityConf.java
@@ -1,4 +1,4 @@
-package com.kunghsuspringcloud.eurekaserver.config;
+package cn.com.kun.springcloud.eurekaserver.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
